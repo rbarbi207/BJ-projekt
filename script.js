8dd6cb3ef9dcb2e9dcf0d658281ce511425329ae
Rozs Barbara <rozs.barbara@students.jedlik.eu>
2024-12-04 11:18:47 +0100

    főoldal script

--- script.js
+++ script.js
@@ -76,3 +76,43 @@ function Card(){
 */
 
 }
+
+let oldalszam = 1
+
+function vissza() {
+    if(oldalszam != 1) {
+        oldalszam--;
+        oldalValtozas(oldalszam)
+    }
+}
+
+function tovabb() {
+    if (oldalszam != 4) {
+        oldalszam++;
+        oldalValtozas(oldalszam)
+    }
+}
+
+function oldalValtozas(oldal){
+    if(oldal == 1){
+        document.getElementById("szabaly").innerHTML = "<li>A blackjack minden leosztásában a cél az osztó megverése. Ehhez olyan lapodnak kell lennie, ami magasabb az osztó lapjánál, de összességében nem haladja meg a 21-et. Emellett akkor is nyerhetsz, ha 22-nél alacsonyabb értékű lapod van, és az osztó lapjának értéke meghaladja a 21-et. Ha a lapod teljes értéke 22 vagy több, azt általában „besokallás”-nak nevezik, és automatikusan vesztessz egy életet.</li>"+
+        "<li>Amikor a kezed értéke megegyezik az osztóéval, azt döntetlen-nek hívják. Ha ez történik, akkor senkisem veszít életet és kezdődik újra a kör.</li>";
+    }
+    else if(oldal == 2){
+        document.getElementById("szabaly").innerHTML = "<li>A blackjackben a tízesek, bubik, dámák, és királyok mindegyikének tízes az értéke. Az ászoknak két különböző értéke lehet, egy vagy tizenegy (választhatsz, melyik).</li>"+
+        "<p>Feladás</p>"+
+        "<li>Néhány blackjackjátékban a feladás lehetővé teszi, hogy eldobd a lapodat, és visszakapd a kezdeti       téted 50%-át, ha úgy érzed, hogy biztosan veszteni fogsz az osztó ellen, feltéve, ha az általad     játszott variánsban a feladás megengedett.</li>";
+    }
+    else if(oldal == 3){
+        document.getElementById("szabaly").innerHTML = "<p>Kettéosztás</p>"+
+        "<li>Lényegében a kettéosztás egy kézből kettőt csinál, így több esélyed lesz nyerni. Amikor kettéosztasz egy lapot, egy további tétet tehetsz az újonnan létrehozott második kézre, ennek értéke megegyezik az eredeti tétével. A kettéosztás a következő helyzetekben fordulhat elő:</li>"+
+        "<ul><li>két egyforma értékű kezdőkártyát kapsz (király-tíz, hat-hat stb.) Ezután mindkét lappal függetlenül játszol, azok a saját értékük szerint nyernek, vesztenek vagy érnek el döntetlent.</li></ul>"
+    }
+    else if(oldal == 4){
+        document.getElementById("szabaly").innerHTML = "<p>Duplázás</p>"+
+        "<li>A duplázáslehetővé teszi, hogy megduplázd az osztónak okozott sebzést, de te is duplán sebzőtsz.</li>"+
+        "<li>Duplázni kettéosztás utén is tudsz de csak egyszer duplázhatsz.</li>"
+    }
+   
+
+}
